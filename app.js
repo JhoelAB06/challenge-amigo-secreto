25d431c157a3f0a37a6bdd44e5ba8bd14ce97115
JhoelAB06 <bushina@fi.uba.ar>
2025-09-23 18:16:07 -0500

    Implementación de la función agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if(nombreAmigo == '') {
+        alert('Por favor, inserte un nombre');
+    } else {
+        listaAmigos.push(nombreAmigo);
+        limpiarCaja();
+    };
+    
+    console.log(nombreAmigo);
+    console.log(listaAmigos);
+
+    return;
+};
+
+function limpiarCaja() {
+    let valorCaja = document.getElementById('amigo');
+    valorCaja.value = '';
+    return;
+};
